89e7a1dc33dfc947c0b4165f6f72e9d451c05ae8
Diego Torres <warorface@gmail.com>
2016-05-25 17:59:11 +0200

    Added new tests and modified to conform better to new way

--- spec/plain_resources_spec.rb
+++ spec/plain_resources_spec.rb
@@ -1,63 +1,135 @@
 require 'cocoapods'
-require 'cocoapods-prune-localizations'
 require 'claide/command/plugin_manager'
 
 CLAide::Command::PluginManager.load_plugins('cocoapods')
 Pod::Config.instance.silent = true
 
-def generate_podfile
+def generate_podfile(localizations = [], used_pods = [])
     Pod::Podfile.new do
-        platform :ios
-        plugin 'cocoapods-prune-localizations', {:localizations => ["en", "es"]}
+        opts = {}
+        opts[:localizations] = localizations unless localizations.empty?
+        platform :ios, '9.0'
+        plugin 'cocoapods-prune-localizations', opts
         
         target 'iOSTarget' do
-            pod 'FormatterKit'
+            used_pods.each do |pod_dict|
+                name = pod_dict[:name]
+                opts = {}
+                opts[:path] ||= pod_dict[:path]
+                
+                pod name, opts
+            end
         end
     end
 end
 
+def lproj_in_bundle(bundle_path)
+    lprojs = []
+    Dir.foreach(bundle_path) do |filename|
+        if filename.end_with? ".lproj"
+            lprojs << File.basename(filename)
+        elsif filename.end_with? ".bundle"
+            lprojs += lproj_in_bundle(File.join(bundle_path, filename))
+        end
+    end
+    lprojs
+end
+
 describe 'Podfile integration' do
-    before(:all) do
-        @sandbox = Pod::Sandbox.new Dir.mktmpdir("prune-loc")
+    before(:each) do
+        @test_dir = Dir.mktmpdir("prune-loc")
+        @sandbox = Pod::Sandbox.new File.join(@test_dir, "/Pods");
+        FileUtils.cp_r 'spec/fixtures/Dummy Targets', @test_dir
     end
     
-    after(:all) do
+    after(:each) do
         FileUtils.rm_r @sandbox.root
     end
     
-    it 'keeps specified languages' do
+    it 'calls original post_install hook' do
+        Dir.chdir(@test_dir) do
+            podfile = generate_podfile
+            
+            called = false
+            podfile.post_install do |installer|
+                called = true
+            end
+            
+            installer = Pod::Installer.new(@sandbox, podfile, nil)
+            installer.installation_options.integrate_targets = false
+            
+            installer.install!
+            
+            expect(called).to be_truthy
+        end
+    end
+    
+    it 'keeps only specified languages in Resources' do
         
-        langs = ["es.lproj", "en.lproj"]
+        pods_to_use = [{:name => "FormatterKit"}]
         
-        FileUtils.cp_r 'spec/fixtures/Dummy Targets', @sandbox.root
-        installer = Pod::Installer.new(@sandbox, generate_podfile, nil)
-        installer.installation_options.integrate_targets = false
-        
-        installer.install!
-        
-        #Verify only en, es resources are available
-        project = Xcodeproj::Project.open(installer.pods_project.path)
-        
-        pod_groups = []
-        pod_items = project["Pods"]
-        dev_pod_items = project["Development Pods"]
-        pod_groups += pod_items.children.objects if pod_items
-        pod_groups += dev_pod_items.children.objects if dev_pod_items
-        
-        pod_groups.each do |group|
+        Dir.chdir(@test_dir) do
+            langs = ["es.lproj", "en.lproj"]
+            podfile = generate_podfile(langs, pods_to_use)
+            
+            installer = Pod::Installer.new(@sandbox, podfile, nil)
+            installer.installation_options.integrate_targets = false
+            
+            installer.install!
+            
+            project = installer.pods_project
+            group = project.pod_group("FormatterKit")
+            
             resGroups = group.recursive_children_groups.select do |group|
                 group.name == "Resources"
             end
-            next unless resGroups.length > 0
+            
+            found_langs = Set.new
             
             resGroups.each do |resGroup|
-                subTrimmedBundlesToAdd = Hash.new
                 resGroup.files.each do |file|
                     if file.path.end_with? ".lproj"
-                        expect(langs).to include(File.basename(file.path))
+                        found_langs.add File.basename(file.path)
                     end
                 end
             end
+            
+            expect(found_langs).to eq langs.to_set
         end
     end
+    
+    it 'keeps only specified languages in Bundles' do
+        
+        pods_to_use = [{:name => "DateTools"}]
+        
+        Dir.chdir(@test_dir) do
+            langs = ["es.lproj", "en.lproj"]
+            podfile = generate_podfile(langs, pods_to_use)
+            
+            installer = Pod::Installer.new(@sandbox, podfile, nil)
+            installer.installation_options.integrate_targets = false
+            
+            installer.install!
+            
+            project = installer.pods_project
+            group = project.pod_group("DateTools")
+            
+            resGroups = group.recursive_children_groups.select do |group|
+                group.name == "Resources"
+            end
+            
+            found_bundles = []
+            resGroups.each do |resGroup|
+                resGroup.files.each do |file|
+                    if file.path.end_with? ".bundle"
+                        found_bundles << file.real_path
+                    end
+                end
+            end
+            
+            found_langs = found_bundles.map {|path| lproj_in_bundle(path)}.flatten!.to_set
+            expect(found_langs).to eq langs.to_set
+        end
+    end
+    
 end
